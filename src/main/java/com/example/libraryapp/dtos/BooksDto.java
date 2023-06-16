0f66ca4e2bd159a4b9fec8c74fe2c9c387a2d268
thifanysouza <thifanyketuli@gmail.com>
2023-06-16 16:13:08 -0300

    Produto final

--- src/main/java/com/example/libraryapp/dtos/BooksDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.libraryapp.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-import java.util.List;
-
-public record BooksDto(@NotBlank String title, List<String>authors, String publisher, String synopsis, Integer year, List<String> subjects) {
-}
